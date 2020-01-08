e65c16f5569a2c007e8418bbec27ff263cc0fae4
gaoyongxiao <gaoyongxiao@bigo.sg>
2020-01-08 14:23:12 +0800

    project init test

---
+++ GanHuo-MVC/GanHuo-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GanHuo-MVC
+//
+//  Created by gaoyongxiao on 2020/1/8.
+//  Copyright © 2020 gaoyongxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
